67eaef0fb4697401e42959dd86cf329f829ae4d7
Fernanda Millán de Urruticoechea <fer.millandeu@gmail.com>
2024-08-11 22:46:16 -0400

    Inicializando repositorio con las carpetas del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar.jsx';
+import Home from './components/Home.jsx';
+import Footer from './components/Footer.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
